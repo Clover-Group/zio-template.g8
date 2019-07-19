2449f392e8aa3cb6738a3ec28280784787735189
tampler <socnetfpga@gmail.com>
2019-07-19 13:15:59 +0300

    Removed $ from tests to avoid sbt new crashes

--- src/main/g8/src/test/scala/HelloSpec.scala
+++ src/main/g8/src/test/scala/HelloSpec.scala
@@ -3,6 +3,6 @@ import org.specs2._
 class HelloSpec extends Specification { def is = s2"""
 
  This is my first specification
-   it is working                 $ok
-   really working!               $ok
+   it is working                 
+   really working!               
                                  """ }
